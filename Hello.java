074e3f1775a11c7855e253597332a25727f565c4
tangp <brillianteyest@163.com>
2017-08-19 20:16:50 +0800

    change Hello.java And HelloB.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello{
 		System.out.println("hello  !!");
 	}
 	public static void info() {
-		System.out.println("hello  !!");
+		System.out.println("你好世界");
 		System.out.println("hello  !!");
 	}
 }
