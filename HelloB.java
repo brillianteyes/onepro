62d5e1e8842a4efef2afbffacba3f65dd6480d46
tangp <brillianteyest@163.com>
2017-08-19 20:05:13 +0800

    delete HelloA.java and HelloB.java

--- HelloB.java
+++
@@ -1,9 +0,0 @@
-public class HelloB{
-	public static void main(String[] args) {
-		System.out.println("hello world !!");
-	}
-	public static void info() {
-		System.out.println("hello  !!");
-		System.out.println("hello  !!");
-	}
-}
