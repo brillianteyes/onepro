dd57c46eb6aadbf83f5e0b41a4b042c7bab1f22b
tangp <brillianteyest@163.com>
2017-08-19 17:33:52 +0800

    修改后提交

--- HelloA.java
+++ HelloA.java
@@ -2,4 +2,8 @@ public class HelloA{
 	public static void main(String[] args) {
 		System.out.println("hello world !!");
 	}
+	public static void info() {
+		System.out.println("hello  !!");
+		System.out.println("hello  !!");
+	}
 }
