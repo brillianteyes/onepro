0c656ad7bdd192edd5e8c0142cbf6af6841f3aeb
tangp <brillianteyest@163.com>
2017-08-19 20:21:43 +0800

    change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("hello world !!");
-		System.out.println("hello  !!");
-		System.out.println("hello  !!");
+		System.out.println(new java.util.Date);
 	}
 	public static void info() {
-		System.out.println("你好世界");
-		System.out.println("hello  !!");
+		System.out.println(new java.util.Date);
 	}
 }
