1685a2b698271a121d32d28ef5a1ea57da754e3e
tangp <brillianteyest@163.com>
2017-08-19 23:01:27 +0800

    midified Hello.java An delete HA,HB,patch

--- Hello.java
+++ Hello.java
@@ -1,31 +1,6 @@
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("hello world !!");
-		
-	}
-	public static void info() {
-		System.out.println("这一行我想项");
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-	}
-	public static void info() {
-		System.out.println("hello");
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
-		System.out.println(new java.util.Date);
+		System.out.println(new java.sql.Date());
+		System.out.pringln(new java.util.Date());
 	}
 }
